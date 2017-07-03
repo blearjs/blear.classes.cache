15adee5283ba29fecd06ba7302144bfaab24c5ee
#云淡然 <cloudcome@163.com>
2017-07-03 15:25:04 +0800

    ~ 单元测试

--- test/test.index.js
+++ test/test.index.js
@@ -17,53 +17,18 @@ describe('测试文件', function () {
         var cache = new Cache();
         var key1 = 'myKey1';
         var key2 = 'myKey2';
-        var key3 = 'myKey3';
-        var key4 = 'myKey4';
         var val1 = {a:1, b:2};
-        var val2 = '{a:1, b:2}';
-        var val3 = '3';
-        var val4 = '4';
 
         cache.set(key1, val1);
-        cache.set(key2, val2);
-        cache.ensure(key1, val3);
-        cache.ensure(key3, val3);
-        cache.replace(key2, val4);
-        cache.replace(key4, val4);
-        var getted1 = cache.get(key1);
-        var getted2 = cache.get(key2);
-        var getted3 = cache.get(key3);
-        var getted4 = cache.get(key4);
-        var exp1 = cache.getExpires(key1);
-        var exp2 = cache.getExpires(key4);
-        var keys = cache.keys();
-        var size = cache.size();
 
-        console.log(keys);
-        expect(keys.indexOf(key1) > -1).toEqual(true);
-        expect(keys.indexOf(key2) > -1).toEqual(true);
-        expect(keys.indexOf(key3) > -1).toEqual(true);
-        expect(keys.length).toEqual(3);
-        expect(size).toEqual(3);
-        expect(getted1).toEqual(val1);
-        expect(getted2).toEqual(val4);
-        expect(getted3).toEqual(val3);
-        expect(getted4).toEqual(null);
-        expect(exp1.getTime() > new Date().getTime()).toEqual(true);
-        expect(exp2.getTime() < new Date().getTime()).toEqual(true);
+        var get1 = cache.get(key1);
+        var get2 = cache.get(key2);
 
+        expect(get1).toEqual(val1);
+        expect(get2).toEqual(null);
 
         cache.remove(key1);
         expect(cache.get(key1)).toEqual(null);
-        expect(cache.has(key1)).toEqual(false);
-        expect(cache.get(key2)).toEqual(getted2);
-        expect(cache.has(key2)).toEqual(true);
-
-
-        cache.clear();
-        expect(cache.get(key1)).toEqual(null);
-        expect(cache.keys().length).toEqual(0);
-        expect(cache.size()).toEqual(0);
     });
 
     it('localStorage', function () {
@@ -72,53 +37,18 @@ describe('测试文件', function () {
         });
         var key1 = 'myKey1';
         var key2 = 'myKey2';
-        var key3 = 'myKey3';
-        var key4 = 'myKey4';
         var val1 = {a:1, b:2};
-        var val2 = '{a:1, b:2}';
-        var val3 = '3';
-        var val4 = '4';
 
         cache.set(key1, val1);
-        cache.set(key2, val2);
-        cache.ensure(key1, val3);
-        cache.ensure(key3, val3);
-        cache.replace(key2, val4);
-        cache.replace(key4, val4);
-        var getted1 = cache.get(key1);
-        var getted2 = cache.get(key2);
-        var getted3 = cache.get(key3);
-        var getted4 = cache.get(key4);
-        var exp1 = cache.getExpires(key1);
-        var exp2 = cache.getExpires(key4);
-        var keys = cache.keys();
-        var size = cache.size();
 
-        console.log(keys);
-        expect(keys.indexOf(key1) > -1).toEqual(true);
-        expect(keys.indexOf(key2) > -1).toEqual(true);
-        expect(keys.indexOf(key3) > -1).toEqual(true);
-        expect(keys.length).toEqual(3);
-        expect(size).toEqual(3);
-        expect(getted1).toEqual(val1);
-        expect(getted2).toEqual(val4);
-        expect(getted3).toEqual(val3);
-        expect(getted4).toEqual(null);
-        expect(exp1.getTime() > new Date().getTime()).toEqual(true);
-        expect(exp2.getTime() < new Date().getTime()).toEqual(true);
+        var get1 = cache.get(key1);
+        var get2 = cache.get(key2);
 
+        expect(get1).toEqual(val1);
+        expect(get2).toEqual(null);
 
         cache.remove(key1);
         expect(cache.get(key1)).toEqual(null);
-        expect(cache.has(key1)).toEqual(false);
-        expect(cache.get(key2)).toEqual(getted2);
-        expect(cache.has(key2)).toEqual(true);
-
-
-        cache.clear();
-        expect(cache.get(key1)).toEqual(null);
-        expect(cache.keys().length).toEqual(0);
-        expect(cache.size()).toEqual(0);
     });
 
     it('sessionStorage', function () {
@@ -127,53 +57,18 @@ describe('测试文件', function () {
         });
         var key1 = 'myKey1';
         var key2 = 'myKey2';
-        var key3 = 'myKey3';
-        var key4 = 'myKey4';
         var val1 = {a:1, b:2};
-        var val2 = '{a:1, b:2}';
-        var val3 = '3';
-        var val4 = '4';
 
         cache.set(key1, val1);
-        cache.set(key2, val2);
-        cache.ensure(key1, val3);
-        cache.ensure(key3, val3);
-        cache.replace(key2, val4);
-        cache.replace(key4, val4);
-        var getted1 = cache.get(key1);
-        var getted2 = cache.get(key2);
-        var getted3 = cache.get(key3);
-        var getted4 = cache.get(key4);
-        var exp1 = cache.getExpires(key1);
-        var exp2 = cache.getExpires(key4);
-        var keys = cache.keys();
-        var size = cache.size();
 
-        console.log(keys);
-        expect(keys.indexOf(key1) > -1).toEqual(true);
-        expect(keys.indexOf(key2) > -1).toEqual(true);
-        expect(keys.indexOf(key3) > -1).toEqual(true);
-        expect(keys.length).toEqual(3);
-        expect(size).toEqual(3);
-        expect(getted1).toEqual(val1);
-        expect(getted2).toEqual(val4);
-        expect(getted3).toEqual(val3);
-        expect(getted4).toEqual(null);
-        expect(exp1.getTime() > new Date().getTime()).toEqual(true);
-        expect(exp2.getTime() < new Date().getTime()).toEqual(true);
+        var get1 = cache.get(key1);
+        var get2 = cache.get(key2);
 
+        expect(get1).toEqual(val1);
+        expect(get2).toEqual(null);
 
         cache.remove(key1);
         expect(cache.get(key1)).toEqual(null);
-        expect(cache.has(key1)).toEqual(false);
-        expect(cache.get(key2)).toEqual(getted2);
-        expect(cache.has(key2)).toEqual(true);
-
-
-        cache.clear();
-        expect(cache.get(key1)).toEqual(null);
-        expect(cache.keys().length).toEqual(0);
-        expect(cache.size()).toEqual(0);
     });
 
     it('#expires', function (done) {
@@ -186,7 +81,6 @@ describe('测试文件', function () {
         var val = 'myVal';
         cache.set(key, val);
         expect(cache.get(key)).toEqual(val);
-        expect(cache.getExpires(key)).toEqual(afterOneSecondsDate);
 
         setTimeout(function () {
             expect(cache.get(key)).toEqual(null);
